56b5c8eee9cda704bb09a7829cd48684724c3c09
Scott Sullivan <scottsullivanltd@gmail.com>
2022-06-13 23:36:13 -0600

    Refacot testing for user request spec

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe "Users Registration" do
 
     post "/api/v1/users", params: request, as: :json
 
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data]
+
     expect(response).to have_http_status(:created)
+    expect(json).to be_a(Hash)
+    expect(data).to have_key(:id)
+    expect(data[:id]).to eq(nil)
+    expect(data).to have_key(:type)
+    expect(data[:type]).to eq("users")
+    expect(data).to have_key(:attributes)
+    expect(data[:attributes]).to be_a(Hash)
+    expect(data[:attributes]).to have_key(:email)
+    expect(data[:attributes]).to have_key(:api_key)
   end
 end
