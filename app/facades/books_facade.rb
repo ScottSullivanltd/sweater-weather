0cf32d912b50cb09141d555e5c3d2a37df63e9a5
Scott Sullivan <scottsullivanltd@gmail.com>
2022-06-14 11:21:29 -0600

    Refactor: all tests pass for 'final' branch = books, Postman response matches expected response

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -1,6 +1,13 @@
 class BooksFacade
   def self.get_location_books(location, quantity)
     book_data = BookService.get_books(location)
-    Book.new(book_data)
+
+    books_found = book_data[:numFound]
+
+    books = []
+    book_data[:docs].first(quantity.to_i).map do |book|
+      books << Book.new(book)
+    end
+    [books, books_found]
   end
 end
--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -10,8 +10,8 @@ class DestinationSerializer
             summary: weather.current_weather.conditions,
             temperature: weather.current_weather.temperature
           },
-          total_books_found: books.total_books_found,
-          books: books.map do |book|
+          total_books_found: books[1],
+          books: books[0].map do |book|
                    {
                      isbn: book.isbn,
                      title: book.title,
